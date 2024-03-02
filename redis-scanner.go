45c710be369220bf6f70c90dd086e4d8d1c753c4
dev@dashxd <125025267+dash-xd@users.noreply.github.com>
2024-03-01 18:10:41 -0800

    Update redis-scanner.go---

--- redis-scanner.go
+++ redis-scanner.go
@@ -1,34 +1,16 @@
+// redis-scanner package
+
 package redis-scanner
 
 import (
 	"context"
 	"fmt"
 	"net/http"
-	"os"
 	"strconv"
 
 	"github.com/dash-xd/redis-scan-callbacks"
 	"github.com/redis/go-redis/v9"
 )
-
-func LazyLoadRedis(client *redis.Client) (*callbacks.Callbacks, error) {
-	fmt.Println("LazyLoading Redis ...")
-	if client == nil {
-		fmt.Println("Initializing Redis client...")
-		client = redis.NewClient(&redis.Options{
-			Addr:     os.Getenv("REDIS_URI"),
-			Password: os.Getenv("REDISCLI_AUTH"),
-			DB:       0,
-		})
-
-		_, err := client.Ping(context.Background()).Result()
-		if err != nil {
-			return nil, fmt.Errorf("error initializing Redis client: %v", err)
-		}
-	}
-
-	return callbacks.NewCallbacks(), nil
-}
 
 func RunScan(pattern string, startingCursor uint64, redisClient *redis.Client, callbackKeys ...string) ([]string, uint64, error) {
 	ctx := context.Background()
@@ -45,7 +27,7 @@ func RunScan(pattern string, startingCursor uint64, redisClient *redis.Client, c
 		for _, key := range keys {
 			for _, callbackKey := range callbackKeys {
 				if callbackFunc, ok := callbacks.CallbackMap[callbackKey]; ok {
-					_, err := callbackFunc(callbacks.NewCallbacks(), redisClient, key)
+					_, err := callbackFunc(redisClient, key)
 					if err != nil {
 						fmt.Printf("error executing callback for key %s: %v\n", key, err)
 					}
@@ -60,7 +42,7 @@ func RunScan(pattern string, startingCursor uint64, redisClient *redis.Client, c
 	return keys, cursor, nil
 }
 
-func ScanHandlerBuilder(env, parentNamespace, childNamespace, entity, pattern string, callbackKeys ...string) http.HandlerFunc {
+func ScanHandlerBuilder(env, parentNamespace, childNamespace, entity, pattern string, redisClient *redis.Client, callbackKeys ...string) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		envParam := r.URL.Query().Get(env)
 		parentNamespaceParam := r.URL.Query().Get(parentNamespace)
